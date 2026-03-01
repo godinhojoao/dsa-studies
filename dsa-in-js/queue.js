85d86afa89ba9623a1d124b35b1364eed18140ec
godinhojoao <godinhojoao2003@gmail.com>
2026-03-01 11:21:38 -0300

    feat: improve queue and stakc in js

--- dsa-in-js/queue.js
+++ dsa-in-js/queue.js
@@ -6,29 +6,43 @@ class QueueNode {
 }
 
 export class Queue {
+  #head;
+  #tail;
+  #size;
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.#head = null;
+    this.#tail = null;
+    this.#size = 0;
   }
 
   insert(val) {
-    if (this.head == null) {
-      this.head = new QueueNode(val);
-      this.tail = this.head;
+    if (this.#head == null) {
+      this.#head = new QueueNode(val);
+      this.#tail = this.#head;
     } else {
-      const tempTail = this.tail;
-      this.tail = new QueueNode(val);
-      tempTail.next = this.tail;
+      const tempTail = this.#tail;
+      this.#tail = new QueueNode(val);
+      tempTail.next = this.#tail;
     }
+    this.#size++;
   }
 
   dequeue() {
-    if (this.head == null) {
+    if (this.#head == null) {
       return -1;
     }
-    const tempHead = this.head;
-    this.head = this.head.next;
+    const tempHead = this.#head;
+    this.#head = this.#head.next;
+    this.#size--;
     return tempHead.val;
+  }
+
+  front() {
+    return this.#head ? this.#head.val : -1;
+  }
+
+  size() {
+    return this.#size;
   }
 }
 
@@ -37,7 +51,11 @@ const queue = new Queue();
 queue.insert(10)
 queue.insert(12)
 queue.insert(13)
+console.log('queue.size()', queue.size()) // 3
+console.log('queue.front()', queue.front()) // 10
 console.log('queue.dequeue()', queue.dequeue()) // 10
 console.log('queue.dequeue()', queue.dequeue()) // 12
 console.log('queue.dequeue()', queue.dequeue()) // 13
 console.log('queue.dequeue()', queue.dequeue()) // -1
+console.log('queue.front()', queue.front()) // -1
+console.log('queue.size()', queue.size()) // 0
--- dsa-in-js/stack.js
+++ dsa-in-js/stack.js
@@ -1,23 +1,36 @@
 export class Stack {
+  #items; // private field
+
   constructor() {
-    this.items = [];
+    this.#items = [];
   }
 
   insert(val) {
-    this.items.push(val);
+    this.#items.push(val);
   }
 
   pop() {
-    return this.items.pop() ?? -1; // -1 means empty
+    return this.#items.pop() ?? -1; // -1 means empty
+  }
+
+  peek() {
+    return this.#items[this.#items.length - 1] ?? -1;
+  }
+
+  size() {
+    return this.#items.length;
   }
 }
 
 const stack = new Stack();
-
 stack.insert(10)
 stack.insert(12)
 stack.insert(13)
+console.log('stack.size()', stack.size())
+console.log('stack.peek()', stack.peek())
 console.log('stack.pop()', stack.pop())
 console.log('stack.pop()', stack.pop())
 console.log('stack.pop()', stack.pop())
 console.log('stack.pop()', stack.pop())
+console.log('stack.peek()', stack.peek())
+console.log('stack.size()', stack.size())
